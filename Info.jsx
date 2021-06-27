696840c1b5445273f453a1cfe04e16c3d5d41f01
deepx28 <86519181+deepx28@users.noreply.github.com>
2021-06-27 15:43:28 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and react.js</h1>
+        <p>A basic Web Dev React Js Bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Python and ML</h1>
+        <p>Introduction to Machine Learning</p>
+      </div>
+      <div className="note">
+        <h1>Python and Cyber Security</h1>
+        <p>Introductiont to Cyber Security</p>
+      </div>
+      <div className="note">
+        <h1>Python and Deep Learning</h1>
+        <p>A basicIntroduction to Deep Learning</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and react.js</h1>
+        <p>A basic Web Dev React Js Bootcamp</p>
+      </div>
+
+      <div className="note">
+        <h1>Javascript and react.js</h1>
+        <p>A basic Web Dev React Js Bootcamp</p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
